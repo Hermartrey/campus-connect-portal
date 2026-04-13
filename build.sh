5a95a8cc60bd6699a2e9cfe5b9aacf953fc633bf
hermart <hermartreyt@gmail.com>
2026-04-13 08:28:00 +0800

    Add build.sh and render.yaml to enable deployment

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+echo "Building frontend..."
+cd frontend
+npm install
+npm run build
+cd ..
+
+echo "Setting up backend..."
+cd backend
+pip install -r requirements.txt
